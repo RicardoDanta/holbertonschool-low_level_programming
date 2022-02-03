37f169c986e696ebbb093759cb399fedf7cba586
RicardoDanta <uruguaydenos@gmail.com>
2022-02-03 12:08:39 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
+#include<stdio.h>
+/**
+ *main - entry
+ *Return:0
+ **/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }

93be6f17edcb26861ded2627c1ff7136d1d576be
RicardoDanta <uruguaydenos@gmail.com>
2022-02-04 13:58:43 -0800

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,11 @@ int main(void)
 
 	e = 'e';
 	q = 'q';
-	{
 	for (low = 'a'; low <= 'z'; low++)
+	{
 		if (low != e && low != q)
 			putchar(low);
 	}
-	putchar("\n");
+	putchar(10);
 	return (0);
 }

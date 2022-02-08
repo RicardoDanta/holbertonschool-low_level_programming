702347d07325157f72a1ae54b00dc923ff132848
RicardoDanta <uruguaydenos@gmail.com>
2022-02-07 16:07:36 -0800

    10

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks for alphabetic character
+ * @c: isalpha
+ * Return: 1 if is a letter, 0 if is otherwise
+ */
+int _isalpha(int c)
+{
+	if 

6c633541cc130f063a198de54a00f369713bc2dc
RicardoDanta <uruguaydenos@gmail.com>
2022-02-09 13:20:09 -0800

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- *print_numbers - abcd
+ *print_numbers - 1234
  */
 void print_numbers(void)
 {
 	char c;
-
 	for (c = 48; c <= 57; c++)
 		_putchar(c);
 	_putchar('\n');

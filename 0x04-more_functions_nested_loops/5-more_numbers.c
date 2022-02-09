372400ecb083fe1c70fc5064331a6d48f10cc61d
RicardoDanta <uruguaydenos@gmail.com>
2022-02-09 13:09:52 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *more_numbers - asdf
+ *
+ * Return:ghjk
+ */
+void more_numbers(void)
+{
+	int num, i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

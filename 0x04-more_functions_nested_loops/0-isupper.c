7fff5e53d5db1ab24a64f716b1894984ff09c13a
RicardoDanta <uruguaydenos@gmail.com>
2022-02-09 07:38:31 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - a function that checks for lowercase character
+ *@c: Algo
+ * Return: 1 if c is uppercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+	else
+		return (0);
+}

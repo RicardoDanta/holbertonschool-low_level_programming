c68d6344b74415fbdafba686763bded0e1190858
RicardoDanta <uruguaydenos@gmail.com>
2022-02-11 15:10:11 -0800

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - a function that prints a string
- * @str: string to print
+ * @s: string to print
  * Return: Always 0
  */
 int _strlen(char *s)
 {
 	char c;
 	int i = 0;
+
 	c = *s;
 
 	while (c != '\0')

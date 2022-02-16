48748afe322b8fdd6586959d3980a1261bbf60d4
RicardoDanta <uruguaydenos@gmail.com>
2022-02-16 10:22:58 -0800

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,20 +2,20 @@
 /**
  *_strcat - a function that concatenates two strings
  *@dest: string
- *@scr: string
+ *@src: string
  * Return: Always 0
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int a = 0, b;
+
 	while (dest[a] != '\0')
 	{
 		a++;
 	}
 	for (b = 0; src[b] != '\0'; b++, a++)
-		{
-			dest[a] = src[b];
-		}
-			return (dest);
+	{
+		dest[a] = src[b];
+	}
+	return (dest);
 }

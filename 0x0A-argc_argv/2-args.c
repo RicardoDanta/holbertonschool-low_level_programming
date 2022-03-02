4abd5c916df1f747f4733173326b2ad7ac46a7ea
RicardoDanta <uruguaydenos@gmail.com>
2022-03-02 09:38:21 -0800

    commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - a program that prints all arguments it receives
- * @argc: argument count
- * @argv: argument value
- * Return: String
+ * @argc: Something
+ * @argv: Something
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int count;
+	int a;
 
-	for (count = 0; count < argc; count++)
-		printf("%s\n", argv[count]);
-
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
 	return (0);
 }

254e4948a5aacc634c4f77d7e5d6df197c0e0589
RicardoDanta <uruguaydenos@gmail.com>
2022-03-02 13:58:24 -0800

    commit

---
+++ 0x09-static_libraries/_strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - drtyu
+ *@s: tyui
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	size_t strlen(const char *s);
+	return (strlen(s));
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - a function that concatenates two strings
+ * @s1: Something
+ * @s2: Something
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	

0027116086b8d6a0ea00c9a250b2291f87ccc1cc
RicardoDanta <uruguaydenos@gmail.com>
2022-03-09 08:50:48 -0800

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Poppy Bob
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

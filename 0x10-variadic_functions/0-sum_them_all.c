7219f35e5d39e5142a6e01275026df5aee60e0b4
RicardoDanta <uruguaydenos@gmail.com>
2022-03-10 12:20:31 -0800

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - a function that returns the sum of all its parameters
- * @n: Something
- * Return: sum
- */
+*sum_them_all - function that returns the sum of all its parameters.
+*@n: n
+*
+*Return: aa
+*/
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum;
+	unsigned int i, sum;
 
-	va_list a;
-
-	va_start(a, n);
-
-	sum = 0;
-
-	for (i = 0; i < n; i++)
-		sum += va_arg(a, unsigned int);
+	va_list(a);
 
 	if (n == 0)
 		return (0);
 
-		va_end(a);
-		return (sum);
+	va_start(a, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(a, const unsigned int);
+
+	va_end(a);
+	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,1 +1,8 @@
+#ifndef VAR_H
+#define VAR_H
+#include <stdio.h>
+#include <stdlib.h>
+
 int sum_them_all(const unsigned int n, ...);
+
+#endif

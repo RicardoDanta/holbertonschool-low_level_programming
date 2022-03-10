71126deb83b73afc345272ec5d47453eeed4010e
RicardoDanta <uruguaydenos@gmail.com>
2022-03-10 11:45:17 -0800

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,11 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list(a);
 	va_start(a, n);
 
-	if (n == 0)
-		return (0);
-
 	for (i = 0, sum = 0; i < n; i++)
-		sum += va_arg(a, unsigned int);
+			sum += va_arg(a, unsigned int);
+
+	if (n == 0)
+	{
+		return (0);
+	}
 
 		va_end(a);
 		return (sum);

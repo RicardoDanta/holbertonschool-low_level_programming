aeb87b931b56055483ff966d4a9bf06df2f9b6c4
RicardoDanta <uruguaydenos@gmail.com>
2022-05-05 16:22:28 -0700

    beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,26 +3,28 @@
 /**
  * add_dnodeint - a function that adds a new node at the beginning
  * @head: Something
- * @newNode: Something
+ * @n: Something
+ * newNode: Something
  * Return: 0
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t = newNode
+	dlistint_t *newNode;
+
 	newNode = malloc(sizeof(dlistint_t));
-	{
 
 	if (!newNode)
 		return (NULL);
 
-		newNode->n = n;
-		newNode->next = head;
-		newNode->prev = NULL;
+	newNode->n = n;
+	newNode->next = *head;
+	newNode->prev = NULL;
 
-		if (*head != NULL)
-			head->prev = newNode;
-			*head = newNode;
-	}
-			return (newNode);
+	if (*head != NULL)
+		(*head)->prev = newNode;
+
+	*head = newNode;
+
+	return (newNode);
 }
-

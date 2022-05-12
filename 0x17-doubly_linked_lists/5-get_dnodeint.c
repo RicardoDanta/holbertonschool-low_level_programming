fb397da72478c218885247674de84d283bcfbe6d
RicardoDanta <uruguaydenos@gmail.com>
2022-05-12 09:29:31 -0700

    node index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - a function that returns the nth node
+ * @head: Something
+ * @index: Something
+ * Return: 0
+ */
+dlistint_t *get_nodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *nd;
+	unsigned int i;
+
+	for (nd = 0; nd < i; nd++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+			head = head->next;
+	}
+			return(head);
+}
